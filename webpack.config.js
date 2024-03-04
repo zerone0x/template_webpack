692dc596deb3faf05bbe126a6921e8d4e924298f
zerone0x <soberzml2333@gmail.com>
2024-03-04 16:49:03 +0800

    🔧 chore(.babelrc): add babel configuration with @babel/preset-env preset 🔧 chore(main.js): update minified code in main.js The changes were made to add a new .babelrc file with the preset configuration for @babel/preset-env to enable modern JavaScript features. Additionally, the minified code in main.js was updated for optimization purposes.
    🚀 chore(package.json): update devDependencies and scripts in package.json
    🔧 chore(webpack.config.js): add babel-loader configuration for JavaScript files
    Updated devDependencies in package.json to include the latest versions of @babel/core, @babel/preset-env, babel-loader, and jest for better compatibility and functionality. Also, updated webpack to version 5.90.3. Added a new configuration in webpack.config.js to use babel-loader for JavaScript files, excluding node_modules and bower_components directories, with @babel/preset-env preset.

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,16 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
     ],
   },
 };
